5bf7a891f6412ba62ba8fe3f22f5ab7535b6fdf4
Roni Tuohino <tuohinoroni@gmail.com>
2024-10-03 20:44:33 +0300

    Add branch deletion cleanup

--- part3/3.04/jobs/generate-todo/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-url=`curl 'https://en.wikipedia.org/wiki/Special:Random' -si | grep -E 'location' | cut -c 11- | tr -d '\r'`
-host="http://todo-api-svc:80/todos"
-
-curl -d """{\"todo\":\"Read ${url}\"}""" -H 'Content-type: application/json' $host
