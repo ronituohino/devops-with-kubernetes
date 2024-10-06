9df5b8f2515d72410f81244f4178dc7d67abf805
Roni Tuohino <tuohinoroni@gmail.com>
2024-10-06 16:43:13 +0300

    Maybe now?

--- part3/3.07/jobs/pg-backup/backup.sh
+++ part3/3.07/jobs/pg-backup/backup.sh
@@ -3,8 +3,7 @@
 DATE=$(date -u +%Y-%m-%d)
 pg_dump --user=$PGUSER --host=postgres-svc --port=5432 --dbname=$PGDATABASE -v > "/usr/$DATE.sql"
 
-curl -sSL https://sdk.cloud.google.com | bash
-PATH=$PATH:/root/google-cloud-sdk/bin
-GOOGLE_APPLICATION_CREDENTIALS="/var/run/secrets/kubernetes.io/serviceaccount/token"
-
-gcloud storage cp /usr/$DATE.sql gs://dwk-pg-backups
+curl -X POST --data-binary @/usr/dump.sql \
+    -H "Authorization: Bearer $GCLOUD_TOKEN" \
+    -H "Content-Type: application/sql" \
+    "https://storage.googleapis.com/upload/storage/v1/b/dwk-pg-backups/o?uploadType=media&name=$DATE.sql"
