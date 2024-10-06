71c09e6f476bcff18086f80536f52c5a88746425
Roni Tuohino <tuohinoroni@gmail.com>
2024-10-06 14:08:06 +0300

    Update host info for backup

--- part3/3.07/jobs/pg-backup/backup.sh
+++ part3/3.07/jobs/pg-backup/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DATE=$(date -u +%Y-%m-%d)
-pg_dump -v http://postgres-svc:5432/ > "/usr/$DATE.sql"
+pg_dump -h http://postgres-svc:5432/ -v > "/usr/$DATE.sql"
 
 curl -sSL https://sdk.cloud.google.com | bash
 PATH=$PATH:/root/google-cloud-sdk/bin
