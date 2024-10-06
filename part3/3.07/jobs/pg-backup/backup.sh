52336997a0e934adf3ab98b111e5256ad377d7c8
Roni Tuohino <tuohinoroni@gmail.com>
2024-10-06 13:35:56 +0300

    Update backup script

--- part3/3.07/jobs/pg-backup/backup.sh
+++ part3/3.07/jobs/pg-backup/backup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-DATE = $(date -u +%Y-%m-%d)
+DATE=$(date -u +%Y-%m-%d)
 pg_dump -v http://postgres-svc:5432/ > "/usr/$DATE.sql"
 
 curl -sSL https://sdk.cloud.google.com | bash
-PATH = $PATH:/root/google-cloud-sdk/bin
+PATH=$PATH:/root/google-cloud-sdk/bin
 
 gcloud storage cp /usr/$DATE.sql gs://dwk-pg-backups
