5db9ed91da2a2fd6db0ed7b4088af90ab31d8b5f
Roni Tuohino <tuohinoroni@gmail.com>
2024-10-06 16:15:36 +0300

    Again trying to fix this...

--- part3/3.07/jobs/pg-backup/backup.sh
+++ part3/3.07/jobs/pg-backup/backup.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 DATE=$(date -u +%Y-%m-%d)
-pg_dump -h postgres://postgres-svc:5432/ -v > "/usr/$DATE.sql"
+pg_dump --user=$PGUSER --host=postgres-svc --port=5432 --dbname=$PGDATABASE -v > "/usr/$DATE.sql"
 
 curl -sSL https://sdk.cloud.google.com | bash
 PATH=$PATH:/root/google-cloud-sdk/bin
-
-gcloud auth activate-service-account --key-file=/var/run/secrets/kubernetes.io/serviceaccount/token
+GOOGLE_APPLICATION_CREDENTIALS="/var/run/secrets/kubernetes.io/serviceaccount/token"
 
 gcloud storage cp /usr/$DATE.sql gs://dwk-pg-backups
